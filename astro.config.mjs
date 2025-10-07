0bc72d7e1c085acddfa9408c2b32a196ce668aa2
Kushagr Arora <kushagrarora.17@gmail.com>
2025-10-07 10:32:08 +0530

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://heartbeatimprov.com",
+
   build: {
     inlineStylesheets: "auto",
   },
+
   compressHTML: true,
+  adapter: netlify(),
 });
